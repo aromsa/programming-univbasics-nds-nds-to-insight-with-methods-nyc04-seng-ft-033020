29393d35e1486b4059e001fffd84df491732b3b2
anna bateman <annacbateman@gmail.com>
2020-03-26 17:22:41 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -24,17 +24,9 @@ end
 
 def directors_totals(nds)
   result = {}
-i = 0
-while i < nds.length do
-  name = nds[i][:name] #iterates over the names in directors_database
-  j = 0 #counter for next while loop
-movies = nds[i][:movies]
-    gross = 0
-    while j < movies.length do
-      gross += movies[j][:worldwide_gross]
-      j += 1
-    end
-    result[name] = gross
+  i = 0
+  while i < nds.length do
+  
     i += 1
   end
   result = {}
